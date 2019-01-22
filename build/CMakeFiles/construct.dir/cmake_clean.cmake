0a921987a7a6dbd66c0c67a2a5c853e7249ce3bb
Hunter Sullivan <hcsullivan12@yahoo.com>
2019-01-21 19:12:38 -0600

    Copy.

---
+++ build/CMakeFiles/construct.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/construct.dir/construct.cxx.o"
+  "CMakeFiles/construct.dir/src/ActionInitialization.cxx.o"
+  "CMakeFiles/construct.dir/src/Analyzer.cxx.o"
+  "CMakeFiles/construct.dir/src/Configuration.cxx.o"
+  "CMakeFiles/construct.dir/src/DetectorConstruction.cxx.o"
+  "CMakeFiles/construct.dir/src/G4Helper.cxx.o"
+  "CMakeFiles/construct.dir/src/MPPCSD.cxx.o"
+  "CMakeFiles/construct.dir/src/MaterialManager.cxx.o"
+  "CMakeFiles/construct.dir/src/OpDetPhotonTable.cxx.o"
+  "CMakeFiles/construct.dir/src/OpticalPhysics.cxx.o"
+  "CMakeFiles/construct.dir/src/PhysicsList.cxx.o"
+  "CMakeFiles/construct.dir/src/PrimaryGeneratorAction.cxx.o"
+  "CMakeFiles/construct.dir/src/Reconstructor.cxx.o"
+  "CMakeFiles/construct.dir/src/SteppingAction.cxx.o"
+  "CMakeFiles/construct.dir/src/Voxel.cxx.o"
+  "CMakeFiles/construct.dir/src/VoxelTable.cxx.o"
+  "CMakeFiles/construct.dir/src/WheelVolume.cxx.o"
+  "construct.pdb"
+  "construct"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/construct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- scripts/analysis/make_class.C
+++
@@ -1,5 +0,0 @@
-void make_class() {
-    TFile *f = new TFile("/home/hunter/projects/Majorana/production/1000000_probabilities.root");
-    TTree *t = (TTree*)f->Get("anatree");
-    t->MakeClass("probabilitiesAna");
-}
